4422c8214a615c76b9e4eae73e6a7a4b9006ac12
Avesh Maurya <aveshmaurya9453@gmail.com>
2026-05-28 10:02:01 +0530

    Configure MongoDB database connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+await connectDB();
+
 let port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
